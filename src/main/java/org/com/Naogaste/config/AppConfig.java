4857db2c8909f0010b28d1bfbe5d7c72ba98b9e6
wellingtonMesquita <wellington88tads@gmail.com>
2021-08-12 23:11:47 -0300

    import s add

--- src/main/java/org/com/Naogaste/config/AppConfig.java
+++ src/main/java/org/com/Naogaste/config/AppConfig.java
@@ -1,10 +1,17 @@
+package org.com.Naogaste.config;
+
+import com.sun.istack.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 @Component
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 public class AppConfig {
     @NotNull
     private String appUrl;
- 
+
     public String getAppUrl() {
         return appUrl;
     }
