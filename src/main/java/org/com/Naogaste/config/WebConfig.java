d84fb76b19862c4663d8ff6c62c463fd20e2ea0c
wellingtonMesquita <wellington88tads@gmail.com>
2021-08-14 21:41:41 -0300

    import s add

--- src/main/java/org/com/Naogaste/config/WebConfig.java
+++ src/main/java/org/com/Naogaste/config/WebConfig.java
@@ -1,26 +1,16 @@
 package org.com.Naogaste.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-
-        return new BCryptPasswordEncoder();
-    }
-
-  public void addCorsMappings(CorsRegistry corsRegistry) {
-    corsRegistry.addMapping("/**")
-      .allowedOrigins("https://naogaste.herokuapp.com")
-      .allowedMethods("*")
-      .maxAge(3600L)
-      .allowedHeaders("*")
-      .exposedHeaders("Authorization")
-      .allowCredentials(true);
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
   }
 }
