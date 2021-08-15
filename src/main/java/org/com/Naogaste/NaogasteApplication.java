c97637079b3370faaca166ee2da2d4800cc7cb9a
wellingtonMesquita <wellington88tads@gmail.com>
2021-08-14 22:39:05 -0300

    ççç

--- src/main/java/org/com/Naogaste/NaogasteApplication.java
+++ src/main/java/org/com/Naogaste/NaogasteApplication.java
@@ -1,12 +1,9 @@
 package org.com.Naogaste;
 
-import org.com.Naogaste.model.Usuario;
 import org.com.Naogaste.repository.UsuarioRepository;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NaogasteApplication {
@@ -18,17 +15,7 @@ public class NaogasteApplication {
 		SpringApplication.run(NaogasteApplication.class, args);
 	}
 
-  @Bean
-  InitializingBean sendDatabase() {
-    return () -> {
-      Usuario usuario =  new Usuario();
-      usuario.setNome("Wellington");
-      usuario.setSenha("12345678");
-      usuario.setAtivo(true);
-      usuario.setLogin("wellington123");
-    usuarioRepository.save(usuario);
-    };
-  }
+
 }
 
 
